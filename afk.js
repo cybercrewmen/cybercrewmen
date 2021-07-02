63768cdb4f4889fb7ba8d0e3438308ff1bb1813b
cybercrewmen <86854873+cybercrewmen@users.noreply.github.com>
2021-07-03 04:42:50 +0800

    Update afk.js

--- afk.js
+++ afk.js
@@ -1,13 +1,1 @@
-module.exports = {
-    name : "afk",
-    description : "idk",
-    execute(message , args){
-        const reason = args.slice(0).join(" ") || "no reason"
-        let user = message.author
-        let member = message.guild.members.cache.get(user.id)
-        
-        member.setNickname(`[AFK]${message.author.username}`).catch(e => {console.error(e);})
-        if(console.error) return message.channel.send({embed : {title : "ERROR", color : "RED",description : "something wrong happen!"}})
-        message.channel.send({embed : {title : "AFK" , color : "GREEN" , description : `**successfully set your afk \n reason : ${reason}**`}})
-    }
-}
+
