e784fc271a127cedbe7a427d6ccc524e583e155b
cybercrewmen <86854873+cybercrewmen@users.noreply.github.com>
2021-07-03 04:53:07 +0800

    Delete copy.js

--- copy.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name : "copy",
-    description : "idk",
-    execute(message, args){
-        const copy = args.slice(0).join (" ")
-        message.channel.send(copy)
-    }
-}
